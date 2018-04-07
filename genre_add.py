858ce8c9b266ba73b0a929111f29f1b54bc1ac4c
mattclifford1 <mattc863@hotmail.com>
2018-04-07 20:06:57 +0100

    sorted large files

--- genre_add.py
+++ genre_add.py
@@ -1,22 +1,38 @@
 import pandas as pd
+from tqdm import tqdm
 
-columns_data = ['title','artist','lat','lon','hot','dance','duration','energy','key','loudness','tempo','year','timbre','time_sig','pitches','tatum','seg_loudness_max','beats','bars','sections','segments','loud_start','loud_time','track_id']
-columns_discogs = ['track_id','discogs_genre']
+data = pd.read_pickle('data2.pkl')
+data['lyrics'] = [0]*len(data)
+data['discogs_genre'] = [0]*len(data)
+lyrics_df = pd.read_pickle('matt_lyrics.pkl')
+discogs_genre = pd.read_pickle('discogs_genre_convert.pkl')
 
-data = pd.read_csv('data2.csv')
-discogs = pd.read_csv('discogs_genre_list.csv')
+for i in tqdm(range(len(lyrics_df))):
+	a = data['track_id'][data['track_id'] == lyrics_df.loc[i,0]]
+	index = int(a.index.get_values())
+	data['lyrics'][index] = lyrics_df.loc[i,1]
+
+for i in tqdm(range(len(discogs_genre))):
+	a = data['track_id'][data['track_id'] == discogs_genre[i][0]]
+	if len(a) > 0:
+		index = int(a.index.get_values())
+		data['discogs_genre'][index] = discogs_genre[i][1]
+
+print(data)
+data.to_pickle('data_with_lyrics_discogs.pkl')
+# data = pd.read_csv('data2.csv')
+# discogs = pd.read_csv('discogs_genre_list.csv')
 
 
-data['discogs_genre'] = discogs['discogs_genre']
+# data['discogs_genre'] = discogs['discogs_genre']
 # data['discogs_one_genre'] = discogs['discogs_genre']
 # for i in range(len(data['discogs_genre'])):
 # 	if data['discogs_one_genre'][i] != '[]':
 # 		data['discogs_one_genre'][i] = data['discogs_genre'][i][0]
 
-print(data)
 
 
 
 
 
-data.to_csv('data_with_discogs.csv',index=False)
+# data.to_csv('data_with_discogs.csv',index=False)
---
+++ just_lyrics.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from tqdm import tqdm
+
+data = pd.read_pickle('data_with_lyrics_discogs.pkl')
+
+for i in tqdm(range(len(data))):
+	if data['lyrics'][i] == 0 or data['discogs_genre'][i] == '':
+		data = data.drop(i)
+
+print(data)
+
+data.to_pickle('lyrics_genre_overlap.pkl')
+
