2442b4390ebcdcd6893521b2cd96724cbd57f311
mattclifford1 <mattc863@hotmail.com>
2018-04-10 16:28:43 +0100

    features to timbvre

--- feature_extract_timeseries.py
+++ feature_extract_timeseries.py
@@ -1,22 +1,68 @@
 import pandas as pd
 from tsfresh import extract_features
 from tqdm import tqdm
-data = pd.read_pickle('data_with_lyrics_discogs.pkl')
 
-#get rid of any songs without a genre
-for i in tqdm(range(len(data))):
-	if data['discogs_genre'][i] == '':
-		data = data.drop(i)
-data.to_pickle('data_only_genre.pkl')
+removed_missing = True
+if removed_missing == False:
+	data = pd.read_pickle('data_with_lyrics_discogs.pkl')
+	#get rid of any songs without a genre
+	for i in tqdm(range(len(data))):
+		if data['discogs_genre'][i] == '':
+			data = data.drop(i)
+	data.to_pickle('data_only_genre.pkl')
 
-data = pd.read_pickle('data_only_genre.pkl')
-#get column names and incicies for future use
-columns = data.columns.tolist()
-indices = data.index.tolist()
+sort_time_series = False
+if sort_time_series == False:
+	#load data with missing genres removed
+	data = pd.read_pickle('data_only_genre.pkl')
+
+	#get column names and indicies for future use
+	columns = data.columns.tolist()
+	indices = data.index.tolist()
+
+	classes = pd.Series(data['discogs_genre'].tolist())
+	classes.to_pickle('classes.pkl', protocol=2)   #for python 2
+
+	time_series = pd.DataFrame()
+
+	features = ['timbre','pitches','tatum','seg_loudness_max','beats','bars','sections','segments','loud_start','loud_time']
+	features = ['timbre']
+	for feat in features:
+		current_feature = data[feat].tolist()
+		dims = current_feature[0].shape[1]
+		for dim in range(dims):  #12 is dim of timbre
+			flat_features = []
+			ids = []
+			# for i in tqdm(range(len(current_feature))):
+			for i in tqdm(range(10)):
+				current_song = current_feature[i]
+				flat_features += list(current_song[:,dim])
+				ids += [i]*len(current_song)
+			time_series[str(dim)] = flat_features
+		time_series['id'] = ids
+
+	time_series.to_pickle('time_series.pkl', protocol=2)  #for python 2
+
+time_series = pd.read_pickle('time_series.pkl')
+classes = pd.read_pickle('classes.pkl')
+from tsfresh import extract_features
+extracted_features = extract_features(time_series, column_id="id")
+import pickle
+pickle.dump(extracted_features, open('extracted_features.pkl', 'wb'))
+# extracted_features = pickle.load(open('extracted_features.pkl', 'rb')
+
+from tsfresh import select_features
+from tsfresh.utilities.dataframe_functions import impute
+
+impute(extracted_features)
+features_filtered = select_features(extracted_features, classes)
+pickle.dump(features_filtered, open('filtered_features.pkl', 'wb'))
+# features_filtered = pickle.load(open('filtered_features.pkl', 'rb')
 
 
-genre = data['discogs_genre'].tolist()
 
 
 
-timbre = data['timbre'].tolist()
+
+
+
